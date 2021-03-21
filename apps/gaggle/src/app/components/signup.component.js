2bae0c9b360a105aa693289bae213806273b6db9
kk9448 <1633100573@qq.com>
2021-03-20 23:38:44 -0400

    add pg and some api test

--- apps/gaggle/src/app/components/signup.component.js
+++ apps/gaggle/src/app/components/signup.component.js
@@ -1,24 +1,48 @@
 import React, { Component } from "react";
 
 export default class SignUp extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {firstName: '',
+         lastName: '',
+         email: '',
+        };
+    
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(event) {
+        const name = event.target.name;
+        this.setState({
+            [name]: event.target.value
+        });
+    }
+    
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.email);
+        event.preventDefault();
+    }
+
+
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <h3>Sign Up</h3>
 
                 <div className="form-group">
                     <label>First name</label>
-                    <input type="text" className="form-control" placeholder="First name" />
+                    <input type="text" className="form-control" placeholder="First name" value={this.state.firstName} onChange={this.handleChange} name="firstName"/>
                 </div>
 
                 <div className="form-group">
                     <label>Last name</label>
-                    <input type="text" className="form-control" placeholder="Last name" />
+                    <input type="text" className="form-control" placeholder="Last name" value={this.state.lastName} onChange={this.handleChange} name="lastName"/>
                 </div>
 
                 <div className="form-group">
                     <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
+                    <input type="email" className="form-control" placeholder="Enter email" value={this.state.email} onChange={this.handleChange} name="email"/>
                 </div>
 
                 <div className="form-group">
