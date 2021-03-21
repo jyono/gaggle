722e9b114ceb07581e084d410a9cffa2efe0e31f
kk9448 <1633100573@qq.com>
2021-03-21 00:16:32 -0400

    Revert "Revert "add pg and some api test""
    This reverts commit e26368a6f212a134951c457029941f9ee11dcea4.

---
+++ apps/api/src/app/modles/index2.js
@@ -0,0 +1,7 @@
+const { Client } = require('pg')
+const client = new Client()
+client.connect()
+client.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
+  console.log(err ? err.stack : res.rows[0].message) // Hello World!
+  client.end()
+})
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,16 +1,58 @@
-import * as express from 'express';
+// import * as express from 'express';
 import { Message } from '@gaggle/api-interfaces';
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 
-const greeting: Message = { message: 'Welcome to api!' };
 
-app.get('/api', (req, res) => {
-  res.send(greeting);
+const db = require("./app/models");
+db.sequelize.sync();
+
+var corsOptions = {
+  origin: "http://localhost:4200"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
 });
 
-const port = process.env.port || 4000;
-const server = app.listen(port, () => {
-  console.log('Listening at http://localhost:' + port + '/api');
+// set port, listen for requests
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
 });
-server.on('error', console.error);
+
+
+
+
+
+
+
+
+
+
+// const greeting: Message = { message: 'Welcome to api!' };
+
+// app.get('/api', (req, res) => {
+//   res.send(greeting);
+// });
+
+// const port = process.env.port || 4000;
+// const server = app.listen(port, () => {
+//   console.log('Listening at http://localhost:' + port + '/api');
+// });
+// server.on('error', console.error);
+
+
+
+
